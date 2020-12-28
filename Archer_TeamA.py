5246ce72095014a71dcd34da8dbcb2508018b913
lczm <zemingchua@gmail.com>
2020-12-28 17:03:24 +0800

    Format and ignore vscode

--- Archer_TeamA.py
+++ Archer_TeamA.py
@@ -6,8 +6,8 @@ from Graph import *
 from Character import *
 from State import *
 
-class Archer_TeamA(Character):
 
+class Archer_TeamA(Character):
     def __init__(self, world, image, projectile_image, base, position):
 
         Character.__init__(self, world, "archer", image)
@@ -38,75 +38,85 @@ class Archer_TeamA(Character):
 
         Character.render(self, surface)
 
-
     def process(self, time_passed):
-        
+
         Character.process(self, time_passed)
-        
-        level_up_stats = ["hp", "speed", "ranged damage", "ranged cooldown", "projectile range"]
+
+        level_up_stats = [
+            "hp",
+            "speed",
+            "ranged damage",
+            "ranged cooldown",
+            "projectile range",
+        ]
         if self.can_level_up():
             choice = randint(0, len(level_up_stats) - 1)
-            self.level_up(level_up_stats[choice])   
+            self.level_up(level_up_stats[choice])
 
 
 class ArcherStateSeeking_TeamA(State):
-
     def __init__(self, archer):
 
         State.__init__(self, "seeking")
         self.archer = archer
 
-        self.archer.path_graph = self.archer.world.paths[randint(0, len(self.archer.world.paths)-1)]
-
+        self.archer.path_graph = self.archer.world.paths[
+            randint(0, len(self.archer.world.paths) - 1)
+        ]
 
     def do_actions(self):
 
         self.archer.velocity = self.archer.move_target.position - self.archer.position
         if self.archer.velocity.length() > 0:
-            self.archer.velocity.normalize_ip();
+            self.archer.velocity.normalize_ip()
             self.archer.velocity *= self.archer.maxSpeed
-
 
     def check_conditions(self):
 
         # check if opponent is in range
         nearest_opponent = self.archer.world.get_nearest_opponent(self.archer)
         if nearest_opponent is not None:
-            opponent_distance = (self.archer.position - nearest_opponent.position).length()
+            opponent_distance = (
+                self.archer.position - nearest_opponent.position
+            ).length()
             if opponent_distance <= self.archer.min_target_distance:
-                    self.archer.target = nearest_opponent
-                    return "attacking"
-        
+                self.archer.target = nearest_opponent
+                return "attacking"
+
         if (self.archer.position - self.archer.move_target.position).length() < 8:
 
             # continue on path
             if self.current_connection < self.path_length:
-                self.archer.move_target.position = self.path[self.current_connection].toNode.position
+                self.archer.move_target.position = self.path[
+                    self.current_connection
+                ].toNode.position
                 self.current_connection += 1
-            
+
         return None
 
     def entry_actions(self):
 
         nearest_node = self.archer.path_graph.get_nearest_node(self.archer.position)
 
-        self.path = pathFindAStar(self.archer.path_graph, \
-                                  nearest_node, \
-                                  self.archer.path_graph.nodes[self.archer.base.target_node_index])
+        self.path = pathFindAStar(
+            self.archer.path_graph,
+            nearest_node,
+            self.archer.path_graph.nodes[self.archer.base.target_node_index],
+        )
 
-        
         self.path_length = len(self.path)
 
-        if (self.path_length > 0):
+        if self.path_length > 0:
             self.current_connection = 0
             self.archer.move_target.position = self.path[0].fromNode.position
 
         else:
-            self.archer.move_target.position = self.archer.path_graph.nodes[self.archer.base.target_node_index].position
+            self.archer.move_target.position = self.archer.path_graph.nodes[
+                self.archer.base.target_node_index
+            ].position
 
 
 class ArcherStateAttacking_TeamA(State):
-
     def __init__(self, archer):
 
         State.__init__(self, "attacking")
@@ -114,7 +124,9 @@ class ArcherStateAttacking_TeamA(State):
 
     def do_actions(self):
 
-        opponent_distance = (self.archer.position - self.archer.target.position).length()
+        opponent_distance = (
+            self.archer.position - self.archer.target.position
+        ).length()
 
         # opponent within range
         if opponent_distance <= self.archer.min_target_distance:
@@ -125,14 +137,16 @@ class ArcherStateAttacking_TeamA(State):
         else:
             self.archer.velocity = self.archer.target.position - self.archer.position
             if self.archer.velocity.length() > 0:
-                self.archer.velocity.normalize_ip();
+                self.archer.velocity.normalize_ip()
                 self.archer.velocity *= self.archer.maxSpeed
-
 
     def check_conditions(self):
 
         # target is gone
-        if self.archer.world.get(self.archer.target.id) is None or self.archer.target.ko:
+        if (
+            self.archer.world.get(self.archer.target.id) is None
+            or self.archer.target.ko
+        ):
             self.archer.target = None
             return "seeking"
 
@@ -144,7 +158,6 @@ class ArcherStateAttacking_TeamA(State):
 
 
 class ArcherStateKO_TeamA(State):
-
     def __init__(self, archer):
 
         State.__init__(self, "ko")
@@ -154,16 +167,17 @@ class ArcherStateKO_TeamA(State):
 
         return None
 
-
     def check_conditions(self):
 
         # respawned
         if self.archer.current_respawn_time <= 0:
             self.archer.current_respawn_time = self.archer.respawn_time
             self.archer.ko = False
-            self.archer.path_graph = self.archer.world.paths[randint(0, len(self.archer.world.paths)-1)]
+            self.archer.path_graph = self.archer.world.paths[
+                randint(0, len(self.archer.world.paths) - 1)
+            ]
             return "seeking"
-            
+
         return None
 
     def entry_actions(self):
